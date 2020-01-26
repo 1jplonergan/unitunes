0f533b37640490d3967917c290ecbf8da1b605b1
WayneShelby <55064227+WayneShelby@users.noreply.github.com>
2020-01-25 21:44:59 -0500

    update Git

--- CUHackit/my-app/src/Album.js
+++ CUHackit/my-app/src/Album.js
@@ -149,3 +149,4 @@ export default function Album() {
     </React.Fragment>
   );
 }
+export default classes
--- CUHackit/my-app/src/App.js
+++ CUHackit/my-app/src/App.js
@@ -12,6 +12,7 @@ import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import classes from '/Album.js';
 
 function Copyright() {
   return (
@@ -140,3 +141,4 @@ export default function SignInSide() {
     </Grid>
   );
 }
+
